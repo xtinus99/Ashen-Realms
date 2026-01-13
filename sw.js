f317758d256df4aa7eefa0175fa589a4e7ef35b4
xtinus99 <Khalid28121999@gmail.com>
2026-01-13 12:11:05 +0000

    Add 'Click me\!' text on easter egg hover

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for The Ashen Realms
 // Enables offline reading of cached content
 
-const CACHE_NAME = 'ashen-realms-v57';
+const CACHE_NAME = 'ashen-realms-v58';
 
 // Core assets to cache immediately on install
 const CORE_ASSETS = [
