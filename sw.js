787fb4b399538f148d841a0f36b186be20b80b97
xtinus99 <Khalid28121999@gmail.com>
2026-01-13 11:56:35 +0000

    Add brightness and glow to easter egg image

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for The Ashen Realms
 // Enables offline reading of cached content
 
-const CACHE_NAME = 'ashen-realms-v54';
+const CACHE_NAME = 'ashen-realms-v55';
 
 // Core assets to cache immediately on install
 const CORE_ASSETS = [
